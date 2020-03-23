fd8d71a4025c6cd9c672ad9e554e28c28be8435d
Amruth Pillai <im.amruth@gmail.com>
2020-03-23 15:53:36 +0530

    make it a pwa!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { register } from './serviceWorker';
 import './tailwind/tailwind.css';
 import './index.css';
 
@@ -16,3 +17,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+register();
