43cb70c1247c695551fb3bae70d687cc3205985d
Amruth Pillai <im.amruth@gmail.com>
2020-03-23 16:20:32 +0530

    update npm scripts

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { register } from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import './tailwind/tailwind.css';
 import './index.css';
 
@@ -18,4 +18,4 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-register();
+serviceWorker.register();
